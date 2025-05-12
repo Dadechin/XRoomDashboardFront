45614c3ca0e401e7166cc3a03f451a87c526e7d0
mi1468 <mahdi1468@gmail.com>
2025-05-12 11:53:03 +0330

    change configs

--- xroom-dashboard/vue.config.js
+++ xroom-dashboard/vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-      disableHostCheck: true,
-
+  devServer: {
+    
+        allowedHosts: 'all',
+  }
 })
-
